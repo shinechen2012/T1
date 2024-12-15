daf580169143a4650e3e8ffc807c38f6b56636ef
shinechen2012 <chenzhonghua77@gmail.com>
2024-12-15 14:15:02 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '32d6f6e8-f7ab-44dc-a941-688e060cc22b';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
